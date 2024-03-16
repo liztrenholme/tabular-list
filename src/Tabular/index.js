3810734b49c888a43c47b846dfb197aa057bc883
liztrenholme <32151271+liztrenholme@users.noreply.github.com>
2024-03-16 01:17:21 -0400

    adds ability to drag rows

--- src/Tabular/index.js
+++ src/Tabular/index.js
@@ -1,19 +1,25 @@
 import './tabular.css';
 import data from './fake-data.json'
-import { useState } from 'react';
+import { useState,useEffect } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
 import fontawesome from '@fortawesome/fontawesome'
 import { faCheckSquare, faCoffee, faArrowUp, faArrowDown } from '@fortawesome/fontawesome-free-solid'
-// import { alphaSort, numSort } from './utils';
+import { 
+    // alphaSort, numSort, onDragStart, onDragOver, onDrop,
+arrayMove } from './utils';
 
 fontawesome.library.add(faCheckSquare, faCoffee, faArrowUp, faArrowDown);
 
 function Tabular() {
     const [activeSort, setActiveSort] = useState(null)
+    const [colWidths, setColWidths] = useState({})
+    const [tableData, setTableData] = useState(data)
+    const [draggedElement, setDraggedElement] = useState(null)
+
     console.log('data...', data)
     const headers = [...new Set(data.map(i => Object.keys(i)).flat())]
     console.log('headers', headers)
-    const processedData = () => {
+    useEffect(() => {
         if (activeSort && activeSort.length) {
             let newD = []
             if (activeSort[1] === 'down') {
@@ -27,30 +33,61 @@ function Tabular() {
                 });
             }
             console.log(newD)
-            // console.log(numSort(activeSort[1], data[activeSort[0]], data))
-            return newD
+            setTableData(newD)
         }
-        return data
+        // setTableData(data)
+      }, [activeSort, tableData, draggedElement]);
+
+    const handleOnDrag = (e, val) => {
+        e.persist();
+        // console.log('dragging...', e, val, e.target)
+        setDraggedElement(e.target._id)
+    }
+    const handleOnDragOver = (e, val) => {
+        e.stopPropagation();
+        e.preventDefault();
+    }
+    const handleOnDrop = (e, val) => {
+        console.log('dropping...', e, val, e.target.parentNode)
+        const dragged = (element) => element._id === draggedElement;
+        const dropped = (element) => element._id === val._id;
+        const draggedEl = tableData.findIndex(dragged)
+        const droppedLoc = tableData.findIndex(dropped)
+        const updatedAfterDrop = arrayMove(tableData, draggedEl, droppedLoc)
+        setTableData(updatedAfterDrop)
+        setDraggedElement(null)
     }
     const handleSetActiveSort = (val, direction) => setActiveSort([val, direction])
-    console.log('active  sort is...', activeSort, processedData()[0].index, processedData()[1].index)
+    console.log('tableData',tableData)
+    console.log('active  sort is...', activeSort, tableData[0].index, tableData[1].index)
     return (
         <div className="tabular-main">
             <table className='tabular-table'>
                 <thead className='tabular-header'>
-                    <tr className='tabular-row'>
-                        {headers.map(val => (<th>{val}
-                            <FontAwesomeIcon icon="fa-solid fa-arrow-up" onClick={() => handleSetActiveSort(val, 'up')} />
-                            <FontAwesomeIcon icon="fa-solid fa-arrow-down" onClick={() => handleSetActiveSort(val, 'down')} />
+                    <tr className='tabular-row-header'>
+                        {headers.map(val => (<th className='tabular-th' width={200}>
+                            <div style={{ display: 'flex', flexDirection: 'row' }}>{val}
+                                <div className='sorting-icons'>
+                                    <FontAwesomeIcon style={{ fontSize: '10px' }} icon="fa-solid fa-arrow-up" onClick={() => handleSetActiveSort(val, 'up')} />
+                                    <FontAwesomeIcon style={{ fontSize: '10px' }} icon="fa-solid fa-arrow-down" onClick={() => handleSetActiveSort(val, 'down')} />
+                                </div>
+                                <div className='col-resize' draggable />
+                            </div>
                         </th>))}
                     </tr>
                 </thead>
                 <tbody className='tabular-body'>
-                    {processedData().map(row => <tr key={Math.random()} className='tabular-row'>
-                        {Object.keys(row).map(d => <td key={Math.random()} className='tabular-cell'>
-                            {typeof row[d] === 'object' ? Object.keys(row[d]).map(p => row[d][p].name || row[d][p]).join(', ') : row[d].toString()}
-                        </td>)}
-                    </tr>)}
+                    {tableData.map(row =>
+                        <tr key={Math.random()}
+                            draggable
+                            className='tabular-row'
+                            onDragStart={(e) => handleOnDrag(e, row)}
+                            onDragOver={(e) => handleOnDragOver(e, row)}
+                            onDrop={(e) => handleOnDrop(e, row)}>
+                            {Object.keys(row).map(d => <td key={Math.random()} className='tabular-cell'>
+                                {typeof row[d] === 'object' ? Object.keys(row[d]).map(p => row[d][p].name || row[d][p]).join(', ') : row[d].toString()}
+                            </td>)}
+                        </tr>)}
                 </tbody>
             </table>
         </div>
--- src/Tabular/utils.js
+++ src/Tabular/utils.js
@@ -1,3 +1,5 @@
+import produce from 'immer'
+
 export const alphaSort = (items, dir, val) => {
     if (dir === 'up') {
     items.sort((a, b) => {
@@ -37,3 +39,71 @@ export const numSort = (dir, a, b) => {
         return b - a;
     }
 }
+
+function getPosFromId(id) {
+  return id.split(",").map((n) => parseInt(n, 10));
+}
+
+export function onDragStart(evt) {
+  evt.persist();
+  //store id as key so onDragOver can read it via "types"
+  evt.dataTransfer.setData(evt.target.id, "");
+}
+
+export function onDragOver(evt) {
+  evt.persist();
+  const fromPos = getPosFromId(evt.dataTransfer.types[0]);
+  const toPos = getPosFromId(evt.target.closest("td").id);
+  if (
+    (fromPos[0] === toPos[0] && fromPos[1] !== toPos[1]) || //horizontal only
+    (fromPos[1] === toPos[1] && fromPos[0] !== toPos[0]) //vertical only
+  ) {
+    evt.preventDefault(); //allow drop
+  }
+}
+
+export function onDrop(evt, setGrid) {
+  evt.preventDefault();
+  const fromPos = getPosFromId(evt.dataTransfer.types[0]);
+  const toPos = getPosFromId(evt.target.closest("td").id);
+  if (fromPos[0] === toPos[0] && fromPos[1] !== toPos[1]) {
+    //horizontal drag
+    const dist = Math.abs(toPos[1] - fromPos[1]);
+    const dir = toPos[1] > fromPos[1] ? 1 : -1; //to right or left
+    setGrid(
+      produce((draft) => {
+        const cell = draft[fromPos[0]][fromPos[1]];
+        for (var i = 0; i < dist; i += 1) {
+          draft[fromPos[0]][fromPos[1] + i * dir] =
+            draft[fromPos[0]][fromPos[1] + (i + 1) * dir];
+        }
+        draft[fromPos[0]][fromPos[1] + dist * dir] = cell;
+      })
+    );
+  } else {
+    //vertical drag
+    const dist = Math.abs(toPos[0] - fromPos[0]);
+    const dir = toPos[0] > fromPos[0] ? 1 : -1; //to down or up
+    setGrid(
+      produce((draft) => {
+        const cell = draft[fromPos[0]][fromPos[1]];
+        for (var i = 0; i < dist; i += 1) {
+          draft[fromPos[0] + i * dir][fromPos[1]] =
+            draft[fromPos[0] + (i + 1) * dir][fromPos[1]];
+        }
+        draft[fromPos[0] + dist * dir][fromPos[1]] = cell;
+      })
+    );
+  }
+}
+
+export function arrayMove(arr, old_index, new_index) {
+    if (new_index >= arr.length) {
+        var k = new_index - arr.length + 1;
+        while (k--) {
+            arr.push(undefined);
+        }
+    }
+    arr.splice(new_index, 0, arr.splice(old_index, 1)[0]);
+    return arr; // for testing
+};
